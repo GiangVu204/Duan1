d88e1817367c15f9cb82052a3fb3a8a663a083d6
LÊ HOÀNG TÚ <lehoangtu56@gmail.com>
2023-11-19 18:55:35 +0700

    Lê Hoàng Tú Welcome , Login

---
+++ app/src/main/java/giangvhph33056/fpoly/duan1/Login.java
@@ -0,0 +1,69 @@
+package giangvhph33056.fpoly.duan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import giangvhph33056.fpoly.duan1.DAO.ThanhVienDAO;
+import giangvhph33056.fpoly.duan1.Model.ThanhVien;
+
+public class Login extends AppCompatActivity {
+    TextInputEditText edtUser,edtPass;
+    AppCompatButton btnLOGIN;
+    ThanhVienDAO tvdao;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edtUser = findViewById(R.id.edtUser);
+        edtPass = findViewById(R.id.edtPass);
+        btnLOGIN = findViewById(R.id.btnLOGIN);
+        CheckBox chkNho = findViewById(R.id.chkNho);
+        // hiên thi tài khoản mật khẩu đã nhớ lêm edt
+        SharedPreferences sharedPreferences = getSharedPreferences("DANGNHAPTT", MODE_PRIVATE);
+        String saveduser = sharedPreferences.getString("MaTV", "");
+        String savedPassword = sharedPreferences.getString("MatKhau", "");
+        if (!savedPassword.isEmpty()) {
+            edtUser.setText(saveduser);
+            edtPass.setText(savedPassword);
+        }
+        //xử lí nút đăng nhập
+        tvdao = new ThanhVienDAO(this);
+        btnLOGIN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edtUser.getText().toString();
+                String pass = edtPass.getText().toString();
+                if(tvdao.checklogin(user,pass)){
+                    /// nhớ tài khoan mật khẩu
+                    if (chkNho.isChecked()==true) {
+                        SharedPreferences sharedPreferences = getSharedPreferences("DANGNHAPTV", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("MaTV", user);
+                        editor.putString("MatKhau", pass); // Lưu mật khẩu vào SharedPreferences
+                        editor.apply();
+
+                    }
+                    Intent intent = new Intent(Login.this,MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(Login.this, "Đăng nhập Thành công", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(Login.this, "Dăng Nhập Thất Bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/giangvhph33056/fpoly/duan1/Model/ThanhVien.java
+++ app/src/main/java/giangvhph33056/fpoly/duan1/Model/ThanhVien.java
@@ -1,7 +1,70 @@
 package giangvhph33056.fpoly.duan1.Model;
 
 public class ThanhVien {
-    private String MaTv;
+    private String MaTV;
     private String HoTen;
+    private String MatKhau;
+    private int SDT;
+    private String Email;
+    private String DChi;
 
+    public ThanhVien() {
+    }
+
+    public ThanhVien(String maTV, String hoTen, String matKhau, int SDT, String email, String DChi) {
+        MaTV = maTV;
+        HoTen = hoTen;
+        MatKhau = matKhau;
+        this.SDT = SDT;
+        Email = email;
+        this.DChi = DChi;
+    }
+
+    public String getMaTV() {
+        return MaTV;
+    }
+
+    public void setMaTV(String maTV) {
+        MaTV = maTV;
+    }
+
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        HoTen = hoTen;
+    }
+
+    public String getMatKhau() {
+        return MatKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        MatKhau = matKhau;
+    }
+
+    public int getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(int SDT) {
+        this.SDT = SDT;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getDChi() {
+        return DChi;
+    }
+
+    public void setDChi(String DChi) {
+        this.DChi = DChi;
+    }
 }

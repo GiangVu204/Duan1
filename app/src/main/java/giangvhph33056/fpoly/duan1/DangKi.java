47ac0423c7266971c495dbe2d7b0240994db7c14
LÊ HOÀNG TÚ <lehoangtu56@gmail.com>
2023-11-20 21:51:34 +0700

    Lê Hoàng Tú

---
+++ app/src/main/java/giangvhph33056/fpoly/duan1/DangKi.java
@@ -0,0 +1,14 @@
+package giangvhph33056.fpoly.duan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DangKi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ki);
+    }
+}

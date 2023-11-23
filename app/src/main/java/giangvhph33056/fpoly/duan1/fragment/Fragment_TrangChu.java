0319aedd0000a831ea0453bf6cf201fd2088f006
LÊ HOÀNG TÚ <lehoangtu56@gmail.com>
2023-11-23 23:20:38 +0700

    Lê Hoàng Tú

--- app/src/main/java/giangvhph33056/fpoly/duan1/fragment/Fragment_TrangChu.java
+++ app/src/main/java/giangvhph33056/fpoly/duan1/fragment/Fragment_TrangChu.java
@@ -1,5 +1,7 @@
 package giangvhph33056.fpoly.duan1.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,21 +9,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import giangvhph33056.fpoly.duan1.R;
 
 public class Fragment_TrangChu extends Fragment {
+    TextView txttennguoidung_tt;
 
     public Fragment_TrangChu() {
-        // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment__trang_chu, container, false);
-
-
+        txttennguoidung_tt = view.findViewById(R.id.txttennguoidung_tt);
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("DANGNHAPTV", Context.MODE_PRIVATE);
+        String loai = sharedPreferences.getString("HoTen","");
+        txttennguoidung_tt.setText(loai);
 
         return view;
     }
